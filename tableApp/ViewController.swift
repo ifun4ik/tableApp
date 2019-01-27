5869cfd42b9839336119afa4e85b9bf3805704c9
Gl1n- <funnydemscreation@gmail.com>
2019-01-27 11:59:09 +0200

    Added the table, setted up all needed interactions

--- tableApp/ViewController.swift
+++ tableApp/ViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var tasks = ["Выгулять Йорика (собаку)", "Покормить рыбок", "Сгонять в универ", "Купить селёдку 😏"]
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var i = 0
-        if (i > 3){
-            print("Add more!")
-            i+=1
-        }
         
     }
 
