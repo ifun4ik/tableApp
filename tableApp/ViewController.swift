8d0ac7d12a4017ca558a6b331a4b0e7feac22a3d
Gl1n- <funnydemscreation@gmail.com>
2019-01-27 11:06:12 +0200

    Hello

--- tableApp/ViewController.swift
+++ tableApp/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var i = 0
+        if (i > 3){
+            print("Add more!")
+            i+=1
+        }
+        
     }
 
 
