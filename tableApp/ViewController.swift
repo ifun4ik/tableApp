aa7c9f7db2433caa730879d5a4184bd427a451a1
Gl1n- <funnydemscreation@gmail.com>
2019-01-27 11:02:03 +0200

    Hello

---
+++ tableApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableApp
+//
+//  Created by Игорь Александров on 27.01.2019.
+//  Copyright © 2019 Alex Glinnikoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
